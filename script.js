6eede0ab2816141dd031628ddf6c3bbcb120779d
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-04-14 03:05:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+let form=document.querySelector("form");
+let btn=document.querySelector("#submit"); 
+let table=document.querySelector("table")
+btn.addEventListener("click",(e)=>{
+	e.preventDefault();
+	let title=document.querySelector("#title").value;
+	let author=document.querySelector("#author").value;
+	let isbn=document.querySelector("#isbn").value;	
+
+	let tr=document.createElement("tr");
+	let td1=document.createElement("td");
+	td1.innerText=title;
+	let td2=document.createElement("td");
+	td2.innerText=author;
+	let td3=document.createElement("td");
+	td3.innerText=isbn;
+	let td4=document.createElement("td").innerText="X";
+	tr.append(td1,td2,td3,td4);
+	table.append(tr);
+	
+})
